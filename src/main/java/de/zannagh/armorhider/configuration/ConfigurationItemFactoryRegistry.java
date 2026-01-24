9d05bcca055fa4f4a7e52a07bf8c988d4f154369
zannagh <234589872+zannagh@users.noreply.github.com>
2026-01-24 09:45:31 +0100

    fix: remove reflection (path-dependent) registration of configuration items

--- src/main/java/de/zannagh/armorhider/configuration/ConfigurationItemFactoryRegistry.java
+++ src/main/java/de/zannagh/armorhider/configuration/ConfigurationItemFactoryRegistry.java
@@ -1,30 +1,18 @@
 package de.zannagh.armorhider.configuration;
 
-import org.jspecify.annotations.NonNull;
+import de.zannagh.armorhider.configuration.items.implementations.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 /**
  * Centralized registry for ConfigurationItemBase factory methods.
- * Scans the implementations package once and creates both single-param and no-arg factories.
+ * Explicitly registers all implementations and creates both single-param and no-arg factories.
  */
 public class ConfigurationItemFactoryRegistry {
 
-    private static final List<String> IMPLEMENTATIONS_PACKAGES = List.of("de.zannagh.armorhider.configuration.items.implementations");
     private static final ConcurrentHashMap<Class<?>, Function<Object, ConfigurationItemBase<?>>> VALUE_FACTORIES = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<Class<?>, Supplier<ConfigurationItemBase<?>>> DEFAULT_FACTORIES = new ConcurrentHashMap<>();
     private static boolean initialized = false;
@@ -34,10 +22,13 @@ public class ConfigurationItemFactoryRegistry {
             return;
         }
 
-        List<Class<? extends ConfigurationItemBase<?>>> implementations = scanForImplementations();
-        for (Class<? extends ConfigurationItemBase<?>> clazz : implementations) {
-            registerFactoriesForClass(clazz);
-        }
+        // Explicitly register all ConfigurationItemBase implementations
+        registerFactoriesForClass(ArmorOpacity.class);
+        registerFactoriesForClass(CombatDetection.class);
+        registerFactoriesForClass(OpacityAffectingElytraItem.class);
+        registerFactoriesForClass(OpacityAffectingHatOrSkullItem.class);
+        registerFactoriesForClass(PlayerName.class);
+        registerFactoriesForClass(PlayerUuid.class);
 
         initialized = true;
     }
@@ -72,8 +63,8 @@ public class ConfigurationItemFactoryRegistry {
 
             if (singleParamConstructor == null) {
                 throw new IllegalStateException(
-                        "ConfigurationItemBase implementation " + clazz.getName() +
-                                " must have a constructor that takes exactly one parameter (the value).");
+                    "ConfigurationItemBase implementation " + clazz.getName() +
+                    " must have a constructor that takes exactly one parameter (the value).");
             }
 
             Constructor<?> finalSingleParamConstructor = singleParamConstructor;
@@ -106,7 +97,7 @@ public class ConfigurationItemFactoryRegistry {
         }
     }
 
-    private static @NonNull Supplier<ConfigurationItemBase<?>> getConfigurationItemBaseSupplier(Class<? extends ConfigurationItemBase<?>> clazz, Constructor<?> noArgConstructor) {
+    private static Supplier<ConfigurationItemBase<?>> getConfigurationItemBaseSupplier(Class<? extends ConfigurationItemBase<?>> clazz, Constructor<?> noArgConstructor) {
         noArgConstructor.setAccessible(true);
         return () -> {
             try {
@@ -115,102 +106,5 @@ public class ConfigurationItemFactoryRegistry {
                 throw new RuntimeException("Failed to create instance of " + clazz.getName(), e);
             }
         };
-    }
-
-    private static List<Class<? extends ConfigurationItemBase<?>>> scanForImplementations() {
-        List<Class<? extends ConfigurationItemBase<?>>> classes = new ArrayList<>();
-
-        try {
-            for (String packageName : IMPLEMENTATIONS_PACKAGES) {
-                ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-                String path = packageName.replace('.', '/');
-                Enumeration<URL> resources = classLoader.getResources(path);
-
-                while (resources.hasMoreElements()) {
-                    URL resource = resources.nextElement();
-
-                    if (resource.getProtocol().equals("file")) {
-                        File directory = new File(resource.toURI());
-                        classes.addAll(findClassesInDirectory(directory, packageName));
-                    } else if (resource.getProtocol().equals("jar")) {
-                        String rawPath = resource.getPath().substring(5, resource.getPath().indexOf("!"));
-                        String jarPath = URLDecoder.decode(rawPath, StandardCharsets.UTF_8);
-                        classes.addAll(findClassesInJar(jarPath, path));
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to scan for ConfigurationItemBase implementations", e);
-        }
-
-        return classes;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static List<Class<? extends ConfigurationItemBase<?>>> findClassesInDirectory(File directory, String packageName) {
-        List<Class<? extends ConfigurationItemBase<?>>> classes = new ArrayList<>();
-
-        if (!directory.exists()) {
-            return classes;
-        }
-
-        File[] files = directory.listFiles();
-        if (files == null) {
-            return classes;
-        }
-
-        for (File file : files) {
-            if (file.isFile() && file.getName().endsWith(".class")) {
-                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-                try {
-                    Class<?> clazz = Class.forName(className);
-                    if (isValidImplementation(clazz)) {
-                        classes.add((Class<? extends ConfigurationItemBase<?>>) clazz);
-                    }
-                } catch (ClassNotFoundException e) {
-                    // Skip classes that can't be loaded
-                }
-            }
-        }
-
-        return classes;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static List<Class<? extends ConfigurationItemBase<?>>> findClassesInJar(String jarPath, String path) {
-        List<Class<? extends ConfigurationItemBase<?>>> classes = new ArrayList<>();
-
-        try (JarFile jar = new JarFile(jarPath)) {
-            Enumeration<JarEntry> entries = jar.entries();
-
-            while (entries.hasMoreElements()) {
-                JarEntry entry = entries.nextElement();
-                String name = entry.getName();
-
-                if (name.startsWith(path) && name.endsWith(".class")) {
-                    String className = name.substring(0, name.length() - 6).replace('/', '.');
-                    try {
-                        Class<?> clazz = Class.forName(className);
-                        if (isValidImplementation(clazz)) {
-                            classes.add((Class<? extends ConfigurationItemBase<?>>) clazz);
-                        }
-                    } catch (ClassNotFoundException e) {
-                        // Skip classes that can't be loaded
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read JAR file: " + jarPath, e);
-        }
-
-        return classes;
-    }
-
-    private static boolean isValidImplementation(Class<?> clazz) {
-        return ConfigurationItemBase.class.isAssignableFrom(clazz) &&
-                !clazz.equals(ConfigurationItemBase.class) &&
-                !Modifier.isAbstract(clazz.getModifiers()) &&
-                !clazz.isInterface();
     }
 }
