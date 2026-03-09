f5a39e2372aaf946dfd437e82f047e08827289b8
zannagh <mail@zannagh.me>
2026-03-09 22:20:46 +0100

    fix: wrong mixin for combat detection and add that attacking mobs will log combat , closes #79 (#118)

--- common/src/client/java/de/zannagh/armorhider/mixin/client/LivingEntityMixin.java
+++ common/src/client/java/de/zannagh/armorhider/mixin/client/LivingEntityMixin.java
@@ -8,10 +8,9 @@ package de.zannagh.armorhider.mixin.client;
 
 import de.zannagh.armorhider.combat.ClientCombatManager;
 import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,21 +19,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
     @Inject(
-            method = "actuallyHurt",
+            method = "handleDamageEvent",
             at = @At(value = "HEAD")
     )
-    //? if > 1.21.1 
-    private void triggerCombat(ServerLevel serverLevel, DamageSource damageSource, float f, CallbackInfo ci) {
-    //? if <= 1.21.1 
-    //private void triggerCombat(DamageSource damageSource, float f, CallbackInfo ci) {
-        if (damageSource.getEntity() == null) {
-            return;
+    private void triggerCombat(DamageSource damageSource, CallbackInfo ci) {
+        Player victim = null;
+        if ((Object) this instanceof AbstractClientPlayer player) {
+            victim = player;
         }
-        if ((Object) this instanceof LocalPlayer localPlayer) {
-            ClientCombatManager.handleCombat(ClientCombatManager::shouldLogCombatForPlayer, damageSource, localPlayer);
-        }
-        if ((Object) this instanceof AbstractClientPlayer remotePlayer) {
-            ClientCombatManager.handleCombat(ClientCombatManager::shouldLogCombatForPlayer, damageSource, remotePlayer);
-        }
+        ClientCombatManager.handleCombat(ClientCombatManager::shouldLogCombatForPlayer, damageSource, victim);
     }
 }
