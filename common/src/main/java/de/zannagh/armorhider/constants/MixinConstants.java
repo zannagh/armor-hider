ad669035c2dae077e261dbc408f91cb3fed28b1c
zannagh <234589872+zannagh@users.noreply.github.com>
2026-04-04 14:14:20 +0200

    oversight

--- common/src/main/java/de/zannagh/armorhider/constants/MixinConstants.java
+++ common/src/main/java/de/zannagh/armorhider/constants/MixinConstants.java
@@ -3,5 +3,4 @@ package de.zannagh.armorhider.constants;
 public final class MixinConstants {
     public final static int DEFAULT_ORDER = 1000;
     public final static int HIGH_PRIO = 500;
-    public final static int HIGHEST_PRIO = 100;
 }
